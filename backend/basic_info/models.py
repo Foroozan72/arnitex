68b29d9c539dcbd5a54f8f5ae48b032156802212
foroozan <foroozankashani0823@gmail.com>
2024-06-05 11:50:15 +0330

    change name of field

--- backend/basic_info/models.py
+++ backend/basic_info/models.py
@@ -1,28 +1,30 @@
 from django.db import models
 from utils.models import TimeStamp
+from django.utils.translation import ugettext_lazy as _
 
 
 class Country(TimeStamp):
-    title = models.CharField(max_length=200)
-    is_show = models.BooleanField(default=True)
+    title = models.CharField(_('Field Name') , max_length=200)
+    is_show = models.BooleanField(_('Field Name') ,default=True)
+
 
     def __str__(self):
         return f"{self.title} - {self.is_show}"
 
 
 class City(TimeStamp):
-    title = models.CharField(max_length=200)
-    is_show = models.BooleanField(default=True)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='cities')
+    title = models.CharField(_('Field Name') ,max_length=200)
+    is_show = models.BooleanField(_('Field Name') ,default=True)
+    country = models.ForeignKey(_('Field Name') ,Country, on_delete=models.CASCADE, related_name='cities')
 
     def __str__(self):
         return f"{self.title} - {self.is_show}"
 
 
 class State(TimeStamp):
-    title = models.CharField(max_length=200 , default = 'nothing')
-    is_show = models.BooleanField(default=True)
-    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='states')
+    title = models.CharField(_('Field Name') ,max_length=200 , default = 'nothing')
+    is_show = models.BooleanField(_('Field Name') , default=True)
+    city = models.ForeignKey(_('Field Name') , City, on_delete=models.CASCADE, related_name='states')
 
     def __str__(self):
         return f"{self.title} - {self.is_show}"
