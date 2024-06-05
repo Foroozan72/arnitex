7fda1b6a7f916a228f515ffd9d2c05175b81e36a
aLi <sabeti.py@gmail.com>
2024-06-05 11:33:33 +0330

    Cleaning codes

--- backend/scripts/import_countries_cities.py
+++ backend/scripts/import_countries_cities.py
@@ -1,19 +1,5 @@
-# import pycountry
-# from basic_info import Country, City
-
-# flag_base_url = "https://raw.githubusercontent.com/lipis/flag-icons/main/flags/4x3/"
-# for country in pycountry.countries:
-#     flag_url = f"{flag_base_url}{country.alpha_2.lower()}.svg"
-#     obj = Country.objects.create(title=country.name, flag=flag_url)
-
-# for subdivision in pycountry.subdivisions:
-#     country = Country.objects.get(title=subdivision.country.name)
-#     City.objects.create(title=subdivision.name, country=country)
-
-print('dwadaw')
-print('dwadaw')
-from basic_info.models import City, Country
 import pycountry
+from basic_info.models import City, Country
 
 flag_base_url = "https://raw.githubusercontent.com/lipis/flag-icons/main/flags/4x3/"
 for country in pycountry.countries:
@@ -23,8 +9,3 @@ for country in pycountry.countries:
 for subdivision in pycountry.subdivisions:
     country = Country.objects.get(title=subdivision.country.name)
     obj, created = City.objects.get_or_create(title=subdivision.name, country=country)
-
-
-print('dwadaw')
-print('dwadaw')
-
