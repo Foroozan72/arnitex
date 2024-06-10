a4655166b0f9f95b62773aa7eda9eea39632cfda
aLi <sabeti.py@gmail.com>
2024-06-10 11:45:30 +0330

    add digital currency model - update migration files

--- backend/basic_info/models.py
+++ backend/basic_info/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from utils.models import TimeStamp
+from utils.models import TimeStamp, UUID
 from django.utils.translation import gettext_lazy as _
 
 
-class Country(TimeStamp):
+class Country(TimeStamp, UUID):
     title = models.CharField( max_length=200, verbose_name=_('Title'))
     is_show = models.BooleanField(default=True, verbose_name=_('Is_show'))
 
@@ -11,7 +11,7 @@ class Country(TimeStamp):
     def __str__(self):
         return f"{self.title} - {self.is_show}"
 
-class City(TimeStamp):
+class City(TimeStamp, UUID):
     title = models.CharField(max_length=200, verbose_name=_('Title'))
     is_show = models.BooleanField(default=True, verbose_name=_('Is_show'))
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='cities', verbose_name=_('Country'))
@@ -19,11 +19,9 @@ class City(TimeStamp):
     def __str__(self):
         return f"{self.title} - {self.is_show}"
 
+class Digitalcurrency(TimeStamp, UUID):
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+    is_show = models.BooleanField(default=True, verbose_name=_('Is show?'))
 
-# class State(TimeStamp):
-#     title = models.CharField(max_length=200 , default = 'nothing', verbose_name=_('Title'))
-#     is_show = models.BooleanField(default=True, verbose_name=_('Is_show'))
-#     city = models.ForeignKey( City, on_delete=models.CASCADE, related_name='states', verbose_name=_('City'))
-
-#     def __str__(self):
-#         return f"{self.title} - {self.is_show}"
+    class Meta:
+        verbose_name, verbose_name_plural = _('Digital Currency'), _('Digital currencies')
