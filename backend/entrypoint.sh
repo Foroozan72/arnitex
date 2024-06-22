c8ea0ac5925b8ee007a4e9b8563850e8e7300c9a
aLi <sabeti.py@gmail.com>
2024-06-22 15:13:50 +0330

    fix bug

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,4 +2,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn daphne -b 0.0.0.0 -p 8000 config.asgi:application
+daphne -b 0.0.0.0 -p 8000 config.asgi:application
