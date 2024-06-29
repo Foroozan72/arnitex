e720752aa9c53f2e7173e79e9613eb5b3b05958b
foroozan <foroozankashani0823@gmail.com>
2024-06-29 14:09:01 +0330

    modify wallet

--- backend/tests/test_wallet.py
+++ backend/tests/test_wallet.py
@@ -48,3 +48,83 @@ class TransferTests(APITestCase):
         self.assertEqual(self.wallet2.balance, 80.0)
         transactions = Transaction.objects.filter(user_wallet__in=[self.wallet1, self.wallet2])
         self.assertEqual(transactions.count(), 2)
+
+
+class TransferTests(APITestCase):
+
+    def setUp(self):
+        # Create users
+        self.user1 = User.objects.create_user(username='user1', password='password')
+        self.user2 = User.objects.create_user(username='user2', password='password')
+
+        # Create wallets for users
+        self.wallet1 = Wallet.objects.create(user=self.user1, balance=100.0)
+        self.wallet2 = Wallet.objects.create(user=self.user2, balance=50.0)
+
+        # Create tokens for authentication
+        self.token1 = Token.objects.create(user=self.user1)
+        self.token2 = Token.objects.create(user=self.user2)
+
+        # Set the URL for the transfer action
+        self.transfer_url = reverse('wallet:wallet-transfer')
+
+        # Set the client to use the first user's token
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token1.key)
+
+    def test_transfer_balance(self):
+        # Data for the transfer
+        data = {
+            'target_wallet_id': self.wallet2.wallet_id,
+            'amount': 30.0
+        }
+
+        # Perform the POST request to transfer the balance
+        response = self.client.post(self.transfer_url, data, format='json')
+
+        # Check that the response status is HTTP 200 OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Refresh wallets from database to get updated balances
+        self.wallet1.refresh_from_db()
+        self.wallet2.refresh_from_db()
+
+        # Assert that the balances are updated correctly
+        self.assertEqual(self.wallet1.balance, 70.0)
+        self.assertEqual(self.wallet2.balance, 80.0)
+
+        # Check that two transaction records were created
+        transactions = Transaction.objects.filter(user_wallet__in=[self.wallet1, self.wallet2])
+        self.assertEqual(transactions.count(), 2)
+
+        # Verify the details of the transactions
+        source_transaction = transactions.get(user_wallet=self.wallet1)
+        self.assertEqual(source_transaction.amount, -30.0)
+        self.assertEqual(source_transaction.sender, self.user1)
+        self.assertEqual(source_transaction.receiver, self.user2)
+
+        target_transaction = transactions.get(user_wallet=self.wallet2)
+        self.assertEqual(target_transaction.amount, 30.0)
+        self.assertEqual(target_transaction.sender, self.user1)
+        self.assertEqual(target_transaction.receiver, self.user2)
+
+    def test_insufficient_balance(self):
+        # Data for the transfer
+        data = {
+            'target_wallet_id': self.wallet2.wallet_id,
+            'amount': 200.0  # More than available balance in wallet1
+        }
+
+        # Perform the POST request to transfer the balance
+        response = self.client.post(self.transfer_url, data, format='json')
+
+        # Check that the response status is HTTP 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Refresh wallets from database to ensure balances haven't changed
+        self.wallet1.refresh_from_db()
+        self.wallet2.refresh_from_db()
+
+        # Assert that the balances are unchanged
+        self.assertEqual(self.wallet1.balance, 100.0)
+        self.assertEqual(self.wallet2.balance, 50.0)
--- backend/wallet/serializers.py
+++ backend/wallet/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 from .models import Transaction, Wallet
+from django.db import transaction
+from django.utils import timezone
+from utils.enums import TransactionActionChoices
+from rest_framework import serializers
+
 
 class WalletSerializer(serializers.ModelSerializer):
     """
@@ -32,48 +37,20 @@ class WalletSerializer(serializers.ModelSerializer):
         return wallet
 
 
-class TransactionSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Transaction model.
-
-    Serializes the user_wallet, action, amount, and timestamp fields.
-
-    Fields:
-    - user_wallet: The wallet associated with the transaction.
-    - action: The action of the transaction.
-    - amount: The amount of the transaction.
-    - timestamp: The timestamp of the transaction.
-    """
-    user_wallet = serializers.SlugRelatedField(slug_field='wallet_id', queryset=Wallet.objects.all())
-
-    class Meta:
-        model = Transaction
-        fields = ['user_wallet', 'action', 'amount', 'timestamp']
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Transaction` instance, given the validated data.
-        """
-        return Transaction.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Transaction` instance, given the validated data.
-        """
-        instance.user_wallet = validated_data.get('user_wallet', instance.user_wallet)
-        instance.action = validated_data.get('action', instance.action)
-        instance.amount = validated_data.get('amount', instance.amount)
-        instance.timestamp = validated_data.get('timestamp', instance.timestamp)
-        instance.save()
-        return instance
-
-
 class TransferSerializer(serializers.Serializer):
     target_wallet_id = serializers.CharField()
     amount = serializers.DecimalField(max_digits=20, decimal_places=8)
 
     def validate(self, data):
-        source_wallet = self.context['source_wallet']
+        request = self.context['request']
+        user = request.user
+        print('user', user)
+        
+        try:
+            source_wallet = Wallet.objects.get(user=user)
+        except Wallet.DoesNotExist:
+            raise serializers.ValidationError("Source wallet does not exist")
+
         try:
             target_wallet = Wallet.objects.get(wallet_id=data['target_wallet_id'])
         except Wallet.DoesNotExist:
@@ -85,5 +62,35 @@ class TransferSerializer(serializers.Serializer):
         if source_wallet == target_wallet:
             raise serializers.ValidationError("Cannot transfer to the same wallet")
 
+        data['source_wallet'] = source_wallet
         data['target_wallet'] = target_wallet
         return data
+
+    def save(self, **kwargs):
+        source_wallet = self.validated_data['source_wallet']
+        target_wallet = self.validated_data['target_wallet']
+        amount = self.validated_data['amount']
+
+        with transaction.atomic():
+            source_wallet.balance -= amount
+            source_wallet.save()
+            target_wallet.balance += amount
+            target_wallet.save()
+
+            Transaction.objects.create(
+                user_wallet=source_wallet,
+                action=TransactionActionChoices.INTERNAL_TRANSFER,
+                amount=-amount,
+                timestamp=timezone.now(),
+                sender=source_wallet.user,
+                receiver=target_wallet.user
+            )
+
+            Transaction.objects.create(
+                user_wallet=target_wallet,
+                action=TransactionActionChoices.INTERNAL_TRANSFER,
+                amount=amount,
+                timestamp=timezone.now(),
+                sender=source_wallet.user,
+                receiver=target_wallet.user
+            )
