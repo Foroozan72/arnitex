6ae19661b393b0033b64778b725ecb54ae65a4c0
aLi <sabeti.py@gmail.com>
2024-07-09 09:09:52 +0330

    Addressed media files - Image field added to user profile

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -239,10 +239,23 @@ class LogoutSerializer(serializers.Serializer):
         token.blacklist()
         return self.validated_data
 
+
+
+
+
+from rest_framework import serializers
+from .models import Profile
+from PIL import Image, ImageDraw, ImageFont
+import os
+from django.conf import settings
+import random
+import string
+import unicodedata
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['user', 'first_name', 'last_name', 'date_of_birth', 'address', 'city', 'state', 'country', 'postal_code', 'national_id']
+        fields = ['user', 'first_name', 'last_name', 'image', 'date_of_birth', 'address', 'city', 'state', 'country', 'postal_code', 'national_id']
         read_only_fields = ['user']
 
     def retrieve(self, instance):
@@ -252,11 +265,69 @@ class ProfileSerializer(serializers.ModelSerializer):
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.date_of_birth = validated_data.get('date_of_birth', instance.date_of_birth)
+        instance.image = validated_data.get('image', instance.image)
         instance.address = validated_data.get('address', instance.address)
         instance.city = validated_data.get('city', instance.city)
         instance.state = validated_data.get('state', instance.state)
         instance.country = validated_data.get('country', instance.country)
         instance.postal_code = validated_data.get('postal_code', instance.postal_code)
         instance.national_id = validated_data.get('national_id', instance.national_id)
+
+        # Generate image with initials if no image is uploaded
+        if instance.first_name and instance.last_name and instance.image == 'profile/image/default.jpg' :
+            initials = self.get_initials(instance.first_name, instance.last_name)
+            image_path = self.generate_initials_image(initials)
+            # instance.image.save(image_path, save=False)
+
         instance.save()
         return instance
+
+    def get_initials(self, first_name, last_name):
+        # Determine the initials based on the language of the characters
+        if self.is_persian(first_name) and self.is_persian(last_name):
+            initials = (last_name[0] + ' ' + first_name[0])
+        else:
+            initials = (last_name[0].upper() + first_name[0].upper())
+        return initials
+
+    def is_persian(self, text):
+        # Check if the text is in Persian
+        for char in text:
+            if 'ARABIC' in unicodedata.name(char):
+                return True
+        return False
+
+    def generate_initials_image(self, initials):
+        # Create an image with initials
+        image_size = (250, 250)
+        background_color = self.get_random_color()
+        text_color = (255, 255, 255)
+        font_size = 100
+
+        image = Image.new('RGB', image_size, color=background_color)
+        draw = ImageDraw.Draw(image)
+        # استفاده از یک فونت که در دسترس است
+        font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
+        font = ImageFont.truetype(font_path, font_size)
+
+        # Get the size of the text
+        text_bbox = draw.textbbox((0, 0), initials, font=font)
+        text_width = text_bbox[2] - text_bbox[0]
+        text_height = text_bbox[3] - text_bbox[1]
+
+        text_x = (image_size[0] - text_width) / 2
+        text_y = (image_size[1] - text_height) / 2
+        draw.text((text_x, text_y), initials, fill=text_color, font=font)
+
+        # Generate a random filename
+        filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10)) + '.png'
+        image_path = os.path.join(settings.MEDIA_ROOT, 'profile_pics', filename)
+        os.makedirs(os.path.dirname(image_path), exist_ok=True)
+        image.save(image_path)
+
+        # Return relative path to the image
+        return os.path.join('profile_pics', filename)
+
+    def get_random_color(self):
+        # Generate a random color
+        return tuple(random.randint(0, 255) for _ in range(3))
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -184,7 +184,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static'
-MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
