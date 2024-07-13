a9eeb1b45639571876e4d730e24cef3c16d15f74
aLi <sabeti.py@gmail.com>
2024-07-13 13:22:08 +0330

    fix error

--- backend/wallet/admin.py
+++ backend/wallet/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from .models import Asset, BankAccount
-from .models import Coin, Network, Transaction
 # Register your models here.
 
 admin.site.register(Asset)
 admin.site.register(BankAccount)
-admin.site.register(Coin)
-admin.site.register(Network)
-admin.site.register(Transaction)
 # admin.site.register(Transaction)
